326cdb887653dd9d6e67e9b084c3cdea450d27e4
Courtney Regensburger <courtneyregensburger@yahoo.com>
2015-05-14 16:43:49 -0500

    Finally semi-functioning update

--- javascript.js
+++ javascript.js
@@ -1,110 +1,41 @@
-//All clickable points on the board
-var square0;
-var square1;
-var square2;
-var square3;
-var square4;
-var square5;
-var square6;
-var square7;
-var square8;
+//Needed vars
+var player = 1;
 
-//If player X turn, get their input
-function getPlayerXMove(move) {
-      if (move === null) {
-        move = getInput();
-    }
-    return move;
-}
+var xWins = 0;
+var oWins = 0;
+var tie = 0;
 
-//If player O turn, get their input
-function getPlayerOMove(move) {
-      if (move === null) {
-        move = getInput();
-    }
-    return move;
-}
+//table cells
+var cells = ["square0", "square1", "square2", "square3",
+  "square4", "square5", "square6", "square7", "square8"];
 
-//All possible wins for X, if no win, check to see if O did, if not, function tie
-function findPlayerXWinner()
-{
-  if(square0 == " X " && square1 == " X " && square2 == " X ")
-  {
-    alert("Player X Wins!");
-    reset();
-  } else if(square3 == " X " && square4 == " X " && square5 == " X ") {
-    alert("Player X Wins!");
-    reset();
-  } else if(square6 == " X " && square7 == " X " && square8 == " X ") {
-    alert("Player X Wins!");
-    reset();
-  } else if(square0 == " X " && square4 == " X " && square8 == " X ") {
-    alert("Player X Wins!");
-    reset();
-  } else if(square6 == " X " && square4 == " X " && square2 == " X ") {
-    alert("Player X Wins!");
-    reset();
-  } else if(square0 == " X " && square3 == " X " && square6 == " X ") {
-    alert("Player X Wins!");
-    reset();
-  } else if(square1 == " X " && square4 == " X " && square7 == " X ") {
-    alert("Player X Wins!");
-    reset();
-  } else if(square2 == " X " && square5 == " X " && square8 == " X ") {
-    alert("Player X Wins!");
-    reset();
+//possible win combinations
+var possibleWins = [["square0","square1","square2" ], 
+  ["square3","square4","square5"], ["square6","square7","square8"],
+  ["square0","square3","square6"], ["square","square4","square7"], 
+  ["square2","square5","square8"], ["square0","square4","square8"], 
+  ["square2","square4","square6"]];
+
+
+//reset the board
+/*function clearStats(one.column clearBtn){
+  xWon = 0;
+  oWon = 0;
+  catsGame = 0;
+  drawStats();
+}*/
+
+/*function clickBtn(btn){
+  if (player == 1){
+    document.getElementByClassName('btn').value = 'X';
+    player -= 1;
   } else {
-    findPlayerOWinner();
-    tieCheck();
+    document.getElementByClassName('btn').value = 'O';
+    player += 1;
   }
-}
+};*/
 
-//All possible wins for O, if no win, check to see if X did, if not, function tie
-function findPlayerOWinner()
-{
-  if(square0 == " O " && square1 == " O " && square2 == " O ")
-  {
-    alert("Player O Wins!");
-    reset();
-  } else if(square3 == " O " && square4 == " O " && square5 == " O ") {
-    alert("Player O Wins!");
-    reset();
-  } else if(square6 == " O " && square7 == " O " && square8 == " O ") {
-    alert("Player O Wins!");
-    reset();
-  } else if(square0 == " O " && square4 == " O " && square8 == " O ") {
-    alert("Player O Wins!");
-    reset();
-  } else if(square6 == " O " && square4 == " O " && square2 == " O ") {
-    alert("Player O Wins!");
-    reset();
-  } else if(square0 == " O " && square3 == " O " && square6 == " O ") {
-    alert("Player O Wins!");
-    reset();
-  } else if(square1 == " O " && square4 == " O " && square7 == " O ") {
-    alert("Player O Wins!");
-    reset();
-  } else if(square2 == " O " && square5 == " O " && square8 == " O ") {
-    alert("Player O Wins!");
-    reset();
-  } else {
-    findPlayerXWinner();
-    tieCheck(); 
-  }
-}
 
-//play three rounds, keeping track of who wins
-function playToThree() {
-    var playerXWins = 0;
-    var playerOWins = 0;
-    var playerTie = 0;
-     if (winner == "Player X") {
-    playerXWins +=1;
-    } else if (winner== "Player O") {
-        playerOWins +=1;
-    } else if (winner=="Tie"){
-      tieWin +=1;
-    }
-  console.log('Player X is at  ' + playerXWins + ' wins. Player O is at ' + playerOWins + 'wins. Total ties are ' + tieWins);
-    return [playerXWins, playerOWins, tieWins];
-}
+
+//something possibly useful
+//document.getElementsByClassName('.two.columns.offset-by-one.column')
