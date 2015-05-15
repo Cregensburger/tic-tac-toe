4b2d8e5b801420935a84e700e2657acc8a450f1e
Courtney Regensburger <courtneyregensburger@yahoo.com>
2015-05-14 21:45:12 -0500

    Working board, time for tweaking

--- javascript.js
+++ javascript.js
@@ -6,36 +6,19 @@ var oWins = 0;
 var tie = 0;
 
 //table cells
-var cells = ["square0", "square1", "square2", "square3",
-  "square4", "square5", "square6", "square7", "square8"];
+var cells = ["cell0", "cell1", "cell2", "cell3",
+  "cell4", "cell5", "cell6", "cell7", "cell8"];
 
 //possible win combinations
-var possibleWins = [["square0","square1","square2" ], 
-  ["square3","square4","square5"], ["square6","square7","square8"],
-  ["square0","square3","square6"], ["square","square4","square7"], 
-  ["square2","square5","square8"], ["square0","square4","square8"], 
-  ["square2","square4","square6"]];
-
-
-//reset the board
-/*function clearStats(one.column clearBtn){
-  xWon = 0;
-  oWon = 0;
-  catsGame = 0;
-  drawStats();
-}*/
-
-/*function clickBtn(btn){
-  if (player == 1){
-    document.getElementByClassName('btn').value = 'X';
-    player -= 1;
-  } else {
-    document.getElementByClassName('btn').value = 'O';
-    player += 1;
-  }
-};*/
+var possibleWins = [["cell0","cell1","cell2" ], 
+  ["cell3","cell4","cell5"], ["cell6","cell7","cell8"],
+  ["cell0","cell3","cell6"], ["cell1","cell4","cell7"], 
+  ["cell2","cell5","cell8"], ["cell0","cell4","cell8"], 
+  ["cell2","cell4","cell6"]];
 
 
 
 //something possibly useful
 //document.getElementsByClassName('.two.columns.offset-by-one.column')
+
+
